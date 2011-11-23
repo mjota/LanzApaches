169988ddebb3dfc45b9f1fc0c4c05cd384f8cb48
Manuel Pol <manuel.pol@gmail.com>
2011-11-23 23:45:36 +0100

    Versión inicial

---
+++ Principal.py
@@ -0,0 +1,108 @@
+'''
+Created on 16/11/2011
+
+@author: manuel
+'''
+import commands
+import random
+from gi.repository import Gtk
+
+class main:
+    def __init__(self):
+        #Crea la ventana de trabajo principal y obtiene los objetos en Glade
+        builder = Gtk.Builder();
+        builder.add_from_file("ventana.glade")
+        
+        dict = {"on_Salir_Clicked": self.Salir,
+                "on_Test_Clicked": self.Test,
+                "on_pruebas_clicked": self.Pruebas,
+                "on_click_about": self.Acerca}
+        
+        builder.connect_signals(dict)
+        
+        self.textoResultado1 = builder.get_object("textview1")
+        self.textoResultado2 = builder.get_object("textview2")
+        self.textoResultado3 = builder.get_object("textview3")
+        self.textoFinal = builder.get_object("textview4")
+        self.entryCargas1 = builder.get_object("entry1")
+        self.entryCargas2 = builder.get_object("entry2")
+        self.entryCargas3 = builder.get_object("entry3")
+        #self.selCarga = builder.get_object("comboboxtext1")
+        self.resultlist = builder.get_object("liststore1")
+        self.about = builder.get_object("aboutdialog1")
+        self.Trequest=[]  
+        
+    def Salir(self,widget):
+        print "He presionado el boton salir"
+        
+        Gtk.main_quit()
+        
+    def Acerca(self,widget):
+        self.about.run()
+        self.about.hide()
+    
+    def Pruebas(self,widget):
+        #self.Lanzador("http://130.206.134.121/carga1.php")
+        #self.Lanzador("http://130.206.134.121/carga1.php")
+        
+        #TotReq = 0.0
+        #for el in self.Trequest:
+        #TotReq = TotReq + el
+        #print TotReq
+        self.resultlist.append(["Hola"])
+        self.resultlist.append(["Adios"])
+        self.resultlist.append(["Denuevo"])
+        #self.textoFinal.get_buffer().set_text("Tiempo de respuesta: " + str(5) + "ms \nPrueba" + str(3))
+        
+    def Test(self,widget):
+        print "He presionado el boton de test1"
+        
+        a = float(self.entryCargas1.get_buffer().get_text())
+        b = float(self.entryCargas2.get_buffer().get_text())
+        c = float(self.entryCargas3.get_buffer().get_text())
+        #sel = self.selCarga.get_active_text()
+        
+        tot = a+b+c
+        a = int((100/tot)*a)
+        b = int((100/tot)*b)
+        c = int((100/tot)*c)
+        
+        TotLan = a+b+c
+        
+        while ((a>0) or (b>0) or (c>0)):
+            ran = random.randint(1,3)
+            if (ran==1 and a>0):
+                self.Lanzador("http://130.206.134.121/carga1.php")
+                a -=1
+            elif (ran==2 and b>0):
+                self.Lanzador("http://130.206.134.121/carga2.php")
+                b -=1
+            elif (ran==3 and c>0):
+                self.Lanzador("http://130.206.134.121/carga2.php")
+                c -=1               
+        
+        TotReq = 0.0
+        for el in self.Trequest:
+            TotReq = TotReq + el
+        Final = TotReq / TotLan 
+        self.resultlist.append(["Tiempo de respuesta: " + str(TotReq)])
+        self.resultlist.append(["Numero de peticiones: " + str(TotLan)])
+        self.resultlist.append(["Productividad: " + str(Final)])
+         
+    def Lanzador(self,link):
+        Comando = "ab -k -n 1 " + link
+        result = commands.getoutput(Comando)
+        self.textoResultado1.get_buffer().set_text(result)
+        tupla = result.split("Time per request:       ")
+        tupla = tupla[1].split(" [ms]")
+        self.Trequest.append(float(tupla[0]))         
+        
+        
+        
+
+if __name__ == '__main__':
+    main()
+    Gtk.main()
+           
+
+    
