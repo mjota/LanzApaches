5088406d3e031dd52aa9db625e879ef7c1893dc8
Manuel Pol <manuel.pol@gmail.com>
2011-11-24 17:35:49 +0100

    Licencia y demás.

--- Principal.py
+++ Principal.py
@@ -1,8 +1,29 @@
-'''
-Created on 16/11/2011
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# LanzApaches - Lanzador de comandos ab para AESI
+# Copyright (c) 2011 - Manuel Joaquin Díaz Pol
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#==============================================================================
+#
+# LanzApaches - Lanzador de comandos ab para AESI
+#
+# @author : Manuel Joaquin Díaz Pol
+# @date   : November 2011
 
-@author: manuel
-'''
 import commands
 import random
 from gi.repository import Gtk
